8a7b0f8d26206e99f4ec0a6d48349e26b0b6f26f
kariuki251 <dkariuki2006@gmail.com>
2025-11-15 15:34:18 +0300

    Fix ALLOWED_HOSTS

--- cosmetic_inventory/settings.py
+++ cosmetic_inventory/settings.py
@@ -24,7 +24,7 @@ from decouple import config
 
 SECRET_KEY = config('SECRET_KEY')  # we’ll set this in environment variables
 DEBUG = config('DEBUG', default=False, cast=bool)
-ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=''). split(',')
 
 
 
